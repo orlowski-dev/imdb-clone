2dfb244ccbc9794e1168e150e5b2c080c0e7a08b
orlowski-dev <orlowski.dev@gmail.com>
2023-11-20 14:33:35 +0100

    Added error page

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["m.media-amazon.com"],
+    //domains: ["m.media-amazon.com"],
     minimumCacheTTL: 3600,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+      },
+    ],
   },
 };
 
