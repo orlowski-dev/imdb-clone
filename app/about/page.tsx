860d0849047f379b68ba0d82b87dd23331791f9f
orlowski-dev <orlowski.dev@gmail.com>
2023-11-20 11:05:20 +0100

    Added About page

---
+++ app/about/page.tsx
@@ -0,0 +1,50 @@
+export default function About() {
+  return (
+    <main>
+      <div className="max-w-6xl mx-auto py-4 px-6 text-justify space-y-6">
+        <h1 className="text-4xl font-bold">About</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+          odit corporis harum animi maxime sequi facere dolorum vero non nostrum
+          quis tempora consectetur laudantium cum molestiae, in ratione eius.
+          Nemo! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Maiores odio, labore qui provident sunt nisi adipisci quibusdam
+          necessitatibus earum debitis, magni rerum perferendis repellendus
+          reprehenderit natus asperiores, quisquam sit dolore. Ipsam voluptate
+          labore esse, recusandae repellendus inventore eveniet maxime quas ut
+          obcaecati reiciendis facere sit consequatur explicabo, dolorem
+          perspiciatis sequi! Libero repellendus aliquam harum nemo blanditiis
+          hic aliquid ex nesciunt?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod maxime
+          doloremque qui et reiciendis iure quia minima a fuga fugit aliquid
+          blanditiis doloribus nam voluptates corrupti totam nihil, hic in.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim iste
+          repellat possimus veniam esse aspernatur pariatur? Est suscipit
+          maiores fuga numquam reprehenderit dolor corrupti, expedita eum,
+          quidem officia ipsam facilis! Deleniti neque quis praesentium libero
+          laudantium illo ratione necessitatibus. Labore molestiae explicabo
+          iste distinctio impedit, perspiciatis optio praesentium harum ratione
+          commodi? Qui dolore beatae id neque itaque consequuntur, aliquam
+          blanditiis. Fuga sequi, exercitationem non optio ab ratione cumque?
+          Iste tempore iusto voluptate nemo cum iure id nulla, ratione
+          consequatur perferendis numquam autem nihil dolorum voluptas. Delectus
+          deleniti nemo mollitia. Eveniet. Odit, sed? Corporis repellendus
+          quibusdam dolorem eaque tempora non atque quisquam expedita rem
+          blanditiis omnis rerum, et aliquid corrupti ea reiciendis. Fugit
+          voluptatem a aperiam veniam accusamus commodi perspiciatis possimus.
+          Vitae consequatur quis amet facilis sequi mollitia temporibus dolores
+          quisquam reprehenderit dolor. Quasi, eum numquam eveniet laboriosam
+          cumque perferendis! Laudantium at sapiente labore alias, fugiat velit
+          aperiam quia. Voluptatum, repellat! Eius tempora eos blanditiis atque
+          rem deserunt optio incidunt sunt aliquam facilis perferendis quis
+          tenetur exercitationem nobis, iste nulla eligendi, cumque alias.
+          Veritatis fuga ad quaerat, distinctio velit maiores! Velit.
+        </p>
+      </div>
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <main>
-      <h1>IMDb Clone</h1>
-    </main>
+    <div className="max-w-6xl mx-auto py-4 px-6">
+      <h1 className="text-4xl font-bold">IMDb Clone</h1>
+    </div>
   );
 }
